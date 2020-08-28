2411f8d31d8e2be1d34ca8cda3f61e42a9f3f7f4
Sasank <thapas@beloit.edu>
2020-08-27 18:37:49 -0700

    Refractoring

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function createWindow (width,height) {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, '/public/index.html'))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
